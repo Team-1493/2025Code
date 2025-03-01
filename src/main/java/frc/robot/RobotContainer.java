6745d9ced43ed53358fe6708432604f93dd2e0e7
Team-1493 <falcons1493@gmail.com>
2025-02-28 22:29:24 -0500

    rotate to field element

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -101,12 +101,17 @@ public class RobotContainer {
          stickDriver.button(2).onTrue(new InstantCommand(() 
         -> drivetrain.setTargetHeading(Math.toRadians(-90))));
         stickDriver.button(1).onTrue(new InstantCommand(() ->
-            drivetrain.setTargetHeading( Math.toRadians(179.9))));
+            drivetrain.setTargetHeading( Math.toRadians(54))));
         stickDriver.button(3).onTrue(new InstantCommand(() ->
-            drivetrain.setTargetHeading(Math.toRadians(90))));            
+            drivetrain.setTargetHeading(Math.toRadians(-54))));            
         stickDriver.button(4).onTrue(new InstantCommand(() ->
-            drivetrain.setTargetHeading(Math.toRadians( 0))));      
+            drivetrain.setTargetHeading(Math.toRadians( 0))));   
 
+        stickDriver.pov(90).onTrue(new InstantCommand(() ->
+            drivetrain.setTargetHeading(Math.toRadians( 120))));   
+        stickDriver.pov(270).onTrue(new InstantCommand(() ->
+            drivetrain.setTargetHeading(Math.toRadians( -120))));   
+s
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
